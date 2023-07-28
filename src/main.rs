10cd95cd23846ba644574e2c8fbbc9c7a1f8f83e
Cosmin Turtureanu <cturtureanu@bitdefender.com>
2023-07-28 21:36:34 +0300

    I'm rusting, this is crazy bro

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use std::time::Duration;
+
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i: u8 = 0;
+    let mut factor: i32 = 1;
+    'running: loop {
+        i = ((i as i32 + factor) % 255) as u8;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    factor += 1
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
+                    factor -= 1
+                },
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        canvas.fill_rect(Rect::new(100, 100, 100, 100)).unwrap();
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
